eb0bff9c3af48bb75ffe4eb496c82791dcbec36c
Sean Campbell <seanpc@uw.edu>
2018-11-26 11:26:36 -0800

    empty inventory trigger with sample code to test

--- empty_inventory.sql
+++ empty_inventory.sql
@@ -1,5 +1,5 @@
 CREATE TRIGGER inventory_cart_check
-  AFTER UPDATE ON Inventory_T
+  AFTER UPDATE ON sunbirds.Inventory_T
   FOR EACH ROW EXECUTE PROCEDURE
   empty_cart();
 
@@ -8,9 +8,30 @@ CREATE OR REPLACE FUNCTION empty_cart()
   RETURNS TRIGGER AS $$
 BEGIN
   IF NEW.Quantity <= 0
-  THEN DELETE from CustomerCartLines_T c WHERE c.Products_T_ProductID = NEW.Products_T_ProductID;
+  THEN DELETE from sunbirds.CustomerCartLines_T c WHERE c.Products_T_ProductID = NEW.Products_T_ProductID;
   END IF;
   RETURN NEW;
 END;
 $$
 LANGUAGE 'plpgsql';
+
+INSERT INTO sunbirds.frametype_t(frametypeid, framecolordescription, frameshapedescription)
+VALUES (1, 'Blue', 'Round')
+
+INSERT INTO sunbirds.products_t(productid, productname, productdescription, is_sunglass, productstandardprice, can_be_sold, photourl, frametype_t_frametypeid)
+VALUES (1, 'Sunbricks', 1, true, 100.00, true, 1, 1)
+
+INSERT INTO sunbirds.inventory_t(inventoryid, quantity, products_t_productid) VALUES (1, 1, 1)
+
+INSERT INTO sunbirds.customers_t(customerid, customername, customeraddress, customercity, customerstate, customerpostalcode, customeremail)
+VALUES (1, 'Sean', '1110 Nike', 'San Diego', 'CA', '98105', 'cean.com')
+
+INSERT INTO sunbirds.customercartlines_t(cartlineid, customers_t_customerid, products_t_productid, quantity)
+VALUES (1, 1, 1, 1)
+
+-- SELECT * from sunbirds.inventory_t;
+--SELECT * from sunbirds.customercartlines_t;
+
+UPDATE sunbirds.inventory_t
+SET quantity = 0
+WHERE inventoryid = 1
